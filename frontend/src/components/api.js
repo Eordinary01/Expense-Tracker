a498c4a403f945a484083504a3379ea62b264d8f
Parth <parth.manocha2901@gmail.com>
2024-06-12 22:04:06 +0530

    changes frontend and backend!! vercel

--- frontend/src/components/api.js
+++ frontend/src/components/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://127.0.0.1:8800/api";
+const API_URL = "https://expense-tracker-xi-eight.vercel.app/api";
 
 const register = async (userData) => {
   try {
