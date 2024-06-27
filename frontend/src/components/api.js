e0c6ab6bfb7bb7a9b2ec45bc6743a77c9de62d17
Parth <parth.manocha2901@gmail.com>
2024-06-27 08:54:23 +0530

    changes--url

--- frontend/src/components/api.js
+++ frontend/src/components/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "https://expense-tracker-738d.onrender.com/api";
+const API_URL = "https://expense-tracker-xi-eight.vercel.app/api";
 
 const register = async (userData) => {
   try {
